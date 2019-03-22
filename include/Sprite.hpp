3ccbfad6ef94743c8353e04f5580e4572eafed3c
Igor Beduin <igor.rob.96@gmail.com>
2019-03-21 22:45:20 -0300

    Sprite header finished

--- include/Sprite.hpp
+++ include/Sprite.hpp
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+
 class Sprite {
     public:
+        Sprite();
+        Sprite(std::string file);
+        ~Sprite();
+        void Open(std::string file);
+        void SetClip(int x, int y,
+                     int w, int h);
+        void Render(int x, int y);
+        int GetWidth();
+        int GetHeight();
+        bool IsOpen();
 
     private:
-    
-}
+        SDL_Texture* texture;
+        int width;
+        int height;
+        SDL_Rect clipRect;
+};
