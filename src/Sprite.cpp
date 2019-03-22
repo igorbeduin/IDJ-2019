ca160902731d8c86ce272aeac97cb511450a70db
Igor Beduin <igor.rob.96@gmail.com>
2019-03-21 22:46:00 -0300

    Sprite class implementation done. No tests made.

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -1,1 +1,53 @@
 #include "Sprite.hpp"
+
+
+Sprite::Sprite() {
+    texture = nullptr;
+}
+
+Sprite::Sprite(std::string file) {
+    texture = nullptr;
+    Open(file);
+}
+
+Sprite::~Sprite(){
+    if (texture != nullptr) {
+        SDL_DestroyTexture(texture);
+    };
+}
+
+void Sprite::Open(std::string file) {
+    if (texture != nullptr) {
+        SDL_DestroyTexture(texture);
+    };
+    texture = IMG_LoadTexture(renderer, path); // Renderer deve ser o renderer do Game
+    if (IMG_LoadTexture(renderer, path) == nullptr) {
+        std::cout << "Deu bosta";
+    } 
+    SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+}
+
+void Sprite::SetClip(int x, int y, int w, int h) {
+    clipRect = {x, y, w, h};
+}
+
+void Sprite::Render(int x, int y) {
+    SDL_Rect* dstLoc = {x, y, clipRect.w, clipRect.h};
+    SDL_RenderCopy(renderer, texture, clipRect, dstLoc);
+}
+
+int Sprite::GetWidth() {
+    return width;
+}
+
+int Sprite::GetHeight() {
+    return height;
+}
+
+bool IsOpen() {
+    if (texture != nullptr) {
+        return true;
+    } else {
+        return false;
+    }
+}
