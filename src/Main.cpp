80abdd297f5454d47e5d0e337d73bdc4628b9753
Igor Beduin <igor.rob.96@gmail.com>
2019-03-24 13:58:57 -0300

    Making TODO more explicit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,9 @@
 #include "../include/Game.hpp"
 
-
 int main(int argc, char** argv) {
     Game& game = Game::GetInstance();
     game.Run();
-    // TODO: TRATAR ERROS DE INICIALIZACAO
+    // TODO: TRATAR ERROS DE INICIALIZACAO COM TRY/CATCH
 
     return 0;
 }
