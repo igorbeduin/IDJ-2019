52b5ac9201697bde8ba53e3e8f7cc751a756182e
Igor Beduin <igor.rob.96@gmail.com>
2019-03-25 11:45:39 -0300

    Some defines and couts added for better legibility and debugging.

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,7 +4,16 @@
 #include <SDL2/SDL_mixer.h>
 #include "../include/Game.hpp"
 
-Game* Game::instance = nullptr;
+#define AUDIO_CHUNKSIZE 1024
+#define AUDIO_FREQUENCY MIX_DEFAULT_FREQUENCY
+#define AUDIO_FORMAT MIX_DEFAULT_FORMAT
+#define AUDIO_CHANNELS MIX_DEFAULT_CHANNELS
+#define SOUND_RESOLUTION 32
+
+#define WINDOW_FLAGS 0 // Ex.: SDL_WINDOW_FULLSCREEN
+
+// Static class member initialization
+Game *Game::instance = nullptr;
 
 Game::Game (std::string title, int width, int height) {
     int SDL_ERROR;
@@ -18,21 +27,40 @@ Game::Game (std::string title, int width, int height) {
     }
 
     SDL_ERROR = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
-    if (SDL_ERROR != 0) { // Garantia de inicialização
+    if (SDL_ERROR != 0) { 
+        // Garantia de inicialização
+        std::cout << "SDL_Init falhou";
     } else {
+        std::cout << "SDL_Init iniciou corretamente" << std::endl;
         IMG_ERROR = IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF);
         if (IMG_ERROR == 0){
             std::cout << "IMG_Init nao carregou nenhum loader";
             // Tratar erro depois
         } else {
+            std::cout << "IMG_Init iniciou corretamente" << std::endl;
             MSC_ERROR = Mix_Init(MIX_INIT_FLAC | MIX_INIT_OGG | MIX_INIT_MP3);
-            Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024); // tratar erro
-            Mix_AllocateChannels(32);
-            window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0); // resolver inputs
-            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE); // resolver inputs
+            if (MSC_ERROR == 0) {
+                std::cout << "Mix_Init nao carregou nenhum loader";
+            } else {
+                std::cout << "Mix_Init iniciou corretamente" << std::endl;
+                Mix_OpenAudio(AUDIO_FREQUENCY, AUDIO_FORMAT, AUDIO_CHANNELS, AUDIO_CHUNKSIZE);
+                Mix_AllocateChannels(SOUND_RESOLUTION);
+                window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, WINDOW_FLAGS);
+                if (window == nullptr) {
+                    std::cout << "Falha na criação da janela" <<std::endl;
+                } else {
+                    std::cout << "Window criada com sucesso!" << std::endl;
+                }
+                renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+                if (renderer == nullptr) {
+                    std::cout << "Falha na criação do renderer" << std::endl;
+                } else {
+                    std::cout << "Renderer criado com sucesso!" << std::endl;
+                }
+            }
         }
-        state = new State();
-    }
+    } // End of initialization routine
+    state = new State();
 }
 
 Game::~Game() {
