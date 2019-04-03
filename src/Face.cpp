ca05b035764ed1f283aee6d3a694083af9672fae
Igor Beduin <igor.rob.96@gmail.com>
2019-04-03 00:13:00 -0300

    Fixing constructor with construction list and implementing methods

--- src/Face.cpp
+++ src/Face.cpp
@@ -1,26 +1,36 @@
 #include "../include/Face.h"
 
-Face::Face(GameObject& associated) {
-    // Construir a classe mãe
-    hitpoints = 30;
+Face::Face(GameObject &associated) : Component::Component(associated),
+                                     hitpoints(30)
+{
 }
 
-Face::Damage(int damage) {
+void Face::Damage(int damage)
+{
+    Component* sound;
     hitpoints = hitpoints - damage;
-    if (hitpoints <= 0) {
+    if (hitpoints <= 0)
+    {
         associated.RequestDelete();
-        // Da play no sound se houver um
+        sound = associated.GetComponent("Sound");
+        if (sound != nullptr)
+        {
+        // sound->Sound::Play();
+        }
     }
 }
 
-void Update(float dt) {}
-void Render() {}
+void Face::Update(float dt) {}
+void Face::Render() {}
 
-bool Is(std::string type) {
-    if (type == "Face") {
+bool Face::Is(std::string type)
+{
+    if (type == "Face")
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
-
