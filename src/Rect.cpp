f5824dd6ba88d1e4265b2045420a1c34d56c64a0
Igor Beduin <igor.rob.96@gmail.com>
2019-04-03 18:13:52 -0300

    Implementing Contains method

--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,1 +1,21 @@
 #include "../include/Rect.h"
+
+bool Rect::Contains(float x, float y)
+{
+    if ((x >= this.x)&&(x <= this.x + w)) 
+    {
+        if ((y >= this.y) && (y <= this.y + h))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    else 
+    {
+        return false;
+    }
+}
