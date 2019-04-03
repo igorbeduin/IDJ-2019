2495abb064d320dd38863a3327b9c33e6d484d93
Igor Beduin <igor.rob.96@gmail.com>
2019-04-03 18:14:55 -0300

    Implementing overload operator to Vec2 (not working)

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,1 +1,16 @@
 #include "../include/Vec2.h"
+
+Vec2::Vec2(int x, int y) : x(x)
+                           y(y)
+{
+}
+
+Vec2 Vec2::operator+(Vec2 &b)
+{
+    return Vec2(x + b.x, y + b.y);
+}
+
+Vec2 Vec2::GetRotated(float theta)
+{
+    return Vec2(x, y);
+}
