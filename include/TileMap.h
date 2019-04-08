e4b54dc791e3597069efc100f74b16412504d6d9
Igor Beduin <igor.rob.96@gmail.com>
2019-04-08 01:45:26 -0300

    TileMap methods backbone created. Missing implementation.

--- include/TileMap.h
+++ include/TileMap.h
@@ -2,9 +2,27 @@
 
 #include <iostream>
 
+#include "GameObject.h"
+#include "TileSet.h"
+
 class TileMap : public Component
 {
-  public:
-    
-  private:
-}
+public:
+  TileMap(GameObject &associated, std::string file, TileSet &tileSet);
+  void Load(std::string file);
+  void SetTileSet(TileSet *tileSet);
+  int &At(int x, int y, int z = 0);
+  void Render();
+  void RenderLayer(int layer, int cameraX = 0,
+                   int cameraY = 0);
+  int GetWidth();
+  int GetHeight();
+  int GetDepth();
+
+private:
+  std::vector<int> tileMatrix;
+  TileSet *tileSet;
+  int mapWidth;
+  int mapHeight;
+  int mapDepth;
+};
--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -1,1 +1,70 @@
 #include "../include/TileMap.h"
+
+TileMap::TileMap(GameObject &associated, std::string file, TileSet *tileSet) : Component::Component(associated),
+                                                                               tileSet(tileSet)
+{
+    TileMap::Load(file.c_str());
+}
+
+void TileMap::Load(std::string file)
+{
+    /*
+    # TODO: 
+    Carregar um arquivo de mapa, no formato dado pelo arquivo map/tileMap.txt.
+    Os primeiros três números são as dimensões do mapa: largura, altura e profundidade.
+    Em seguida, vêm os tiles, que devem ser carregados em ordem para a matriz de tiles.
+
+    Note que, para o arquivo que usamos na disciplina, tiles vazios são representados por 0,
+    que é o padrão do editor de tilemaps open spurce TileD.
+    Para o nosso código, é mais conveniente que eles sejam representados por - 1,
+     e o primeiro tile do tileset por 0. Portanto, subtraia um de cada índice lido do arquivo.
+    */
+}
+
+void TileMap::SetTileSet(TileSet* tileSet)
+{
+    this->tileSet = tileSet;
+}
+int& TileMap::At(int x, int y, int z)
+{
+    /*
+    # TODO:
+    ENCAPSULAR A FUNÇÃO ACIMA PRIMEIRO
+    TODO: Calcular índice de retorno [x][y][z] de tileMatrix (que é um vetor).
+    */
+}
+
+void TileMap::RenderLayer(int layer, int cameraX, int cameraY)
+{
+    /*
+    # TODO:
+    Renderiza uma camada do mapa, tile a tile. Note que há dois ajustes a se fazer:
+        * Deve-se compensar o deslocamento da cˆmera
+        * Deve-se considerar o tamanho de cada tile (use os membros GetTileWidth e GetTileHeight de TileSet)
+    Ainda não temos câmera. Você pode deixar para implementar o primeiro ponto naquela oasião, mas já faça a 
+    função recebendo esses argumentos.
+    */
+}
+
+void TileMap::Render()
+{
+    /*
+    # TODO:
+    Tenderiza as camadas do mapa. Dica: utilize o RenderLayer e o box do GameObject que o contém.
+    */
+}
+
+int TileMap::GetWidth()
+{
+    return mapWidth;
+}
+
+int TileMap::GetHeight()
+{
+    return mapHeight;
+}
+
+int TileMap::GetDepth()
+{
+    return mapDepth;
+}
