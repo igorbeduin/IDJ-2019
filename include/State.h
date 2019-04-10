1d927f7a24d2a0e1ef610fe5271ef75514d45484
Igor Beduin <igor.rob.96@gmail.com>
2019-04-10 15:37:14 -0300

    Background is not State member anymore

--- include/State.h
+++ include/State.h
@@ -4,29 +4,28 @@
 #include <memory>
 
 #include "Sprite.h"
+#include "Music.h"
 #include "Sound.h"
 #include "GameObject.h"
 #include "TileMap.h"
 #include "TileSet.h"
+#include "Resources.h"
 
 class State
 {
-  public:
-    State();
-    ~State();
-    bool QuitRequested();
-    void LoadAssets();
-    void Update(float dt);
-    void Render();
+public:
+  State();
+  ~State();
+  bool QuitRequested();
+  void LoadAssets();
+  void Update(float dt);
+  void Render();
 
-  private:
-    GameObject background;
-    TileSet *tileSet;
-    Sprite *bg_sprite;
-    Sound *bg_sound;
-    TileMap *bg_tileMap; 
-    bool quitRequested;
-    void Input();
-    void AddObject(int mouseX, int mouseY);
-    std::vector<std::shared_ptr<GameObject>> objectArray;
+private:
+  Music music;
+  bool quitRequested;
+  void Input();
+  void AddObject(int mouseX, int mouseY);
+  std::vector<std::shared_ptr<GameObject>> objectArray;
+  // static Resources *resources;
 };
