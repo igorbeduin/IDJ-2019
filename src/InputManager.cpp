d63ba668affe09e5c15674db753071f7dd37c1cb
Igor Beduin <igor.rob.96@gmail.com>
2019-04-12 15:56:48 -0300

    First implementation of InputManager functions

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,1 +1,42 @@
 #include "../include/InputManager.h"
+
+static InputManager instance;
+
+InputManager& InputManager::GetInstance()
+{
+    return instance;
+}
+
+InputManager::InputManager() : quitRequested(false),
+                               updateCounter(0)
+{
+    /*
+    TODO:
+        Inicializar:
+        * mouseState
+        * mouseUpdate
+    */
+}
+
+void InputManager::Update()
+{
+    /*
+    TODO:
+        Tratar eventos
+    */
+}
+
+bool InputManager::QuitRequested()
+{
+    return quitRequested;
+}
+
+int InputManager::GetMouseX()
+{
+    return mouseX;
+}
+
+int InputManager::GetMouseY()
+{
+    return mouseY;
+}
