5b42d50fa57aff2db6a43474a2558d61bd6d5656
Igor Beduin <igor.rob.96@gmail.com>
2019-04-12 15:57:18 -0300

    Adding defines for mouse maping in InputManager header

--- include/InputManager.h
+++ include/InputManager.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#define LEFT_ARROW_KEY SDLK_LEFT
+#define RIGHT_ARROW_KEY SDLK_RIGHT
+#define UP_ARROW_EY SDLK_UP
+#define DOWN_ARROW_KEY SDLK_DOWN
+#define ESCAPE_KEY SDLK_ESCAPE
+#define LEFT_MOUSE_BUTTON SDL_BUTTON_LEFT
+
 #include <unordered_map>
 
 class InputManager
@@ -20,7 +27,9 @@ private:
     bool mouseState[6];
     int mouseUpdate[6];
     std::unordered_map<int, bool> keyState;
-    std::unoredered_map<int, int> keyUpdate;
+    std::unordered_map<int, int> keyUpdate;
+    bool quitRequested;
+    int updateCounter;
     int mouseX;
     int mouseY;
 };
