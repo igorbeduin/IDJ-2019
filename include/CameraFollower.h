17c54283bdcc0361eac50fcb7057b35a9031e540
Igor Beduin <igor.rob.96@gmail.com>
2019-05-01 00:38:25 -0300

    CameraFollower implemented

--- include/CameraFollower.h
+++ include/CameraFollower.h
@@ -8,8 +8,8 @@
 class CameraFollower : public Component
 {
 public:
-    CameraFollower(GameObject& go);
+    CameraFollower(GameObject& associated);
     void Update(float dt);
     void Render();
     bool Is(std::string type);
-}
+};
