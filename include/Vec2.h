a252d703c73880aa40d2d48a1b5dcefed64bb4a1
Igor Beduin <igor.rob.96@gmail.com>
2019-05-10 01:51:34 -0300

    Implementing "Distance"method

--- include/Vec2.h
+++ include/Vec2.h
@@ -7,6 +7,7 @@ class Vec2
 public:
   Vec2(int x, int y);
   Vec2();
+  static float Distance(Vec2 begin, Vec2 end);
   Vec2 GetRotated(float theta);
   float x, y;
   Vec2 operator+(const Vec2 &other) const;
