2d93784ed3afbde7bcb82c470e7ce987bacd4d95
Igor Beduin <igor.rob.96@gmail.com>
2019-05-21 18:07:02 -0300

    Creating a new State class

--- include/State.h
+++ include/State.h
@@ -1,51 +1,34 @@
 #pragma once
 
-#define BACKGROUND_SPRITE_PATH "assets/img/ocean.jpg"
-#define BACKGROUND_MUSIC_PATH "assets/audio/stageState.ogg"
-#define BACKGROUND_MUSIC_LOOP_TIMES -1 // -1 for infinite loop
-
-#define ENEMY_SPRITE_PATH "assets/img/penguinface.png"
-#define ENEMY_SOUND_PATH "assets/audio/boom.wav"
-#define PI 3.141592
-
-#define TILE_WIDTH 64
-#define TILE_HEIGHT 64
-#define MAP_TILEMAP_PATH "assets/map/tileMap.txt"
-#define MAP_TILESET_PATH "assets/img/tileset.png"
-
-#include <vector>
 #include <memory>
 
-#include "Sprite.h"
-#include "Music.h"
-#include "Sound.h"
 #include "GameObject.h"
-#include "TileMap.h"
-#include "TileSet.h"
-#include "CameraFollower.h"
-#include "Alien.h"
-#include "PenguinBody.h"
-#include "PenguinCannon.h"
-#include "Collider.h"
-#include "Collision.h"
 
 class State
 {
 public:
-  void Start();
-  State();
-  ~State();
-  bool QuitRequested();
-  void LoadAssets();
-  void Update(float dt);
-  void Render();
-  std::weak_ptr<GameObject> AddObject(GameObject* go);
-  std::weak_ptr<GameObject> GetObjectPtr(GameObject* go);
+    State();
+    ~State();
+    virtual void LoadAssets() = 0;
+    void Update(float dt) = 0;
+    void Render() = 0;
+    void Start() = 0;
+    void Pause() = 0;
+    void Resume() = 0;
+    std::weak_ptr<GameObject> AddObject(GameObject* object) = 0;
+    std::weak_ptr<GameObject> GetObjectPtr(GameObject* object) = 0;
+    bool PopRequested();
+    bool QuitRequested();
 
-private:
-  Music music;
-  bool quitRequested;
-  void Input();
-  std::vector<std::shared_ptr<GameObject>> objectArray;
-  bool started;
+    
+protected:
+    void virtual StartArray();
+    void virtual UpdateArray(float dt);
+    void virtual RenderArray();
+    bool popRequested;
+    bool quitRequested;
+    bool started;
+
+    std::vector<std::shared_ptr<GameObject>> objectArray;
+
 };
