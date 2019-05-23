779741284acf9aa728ac1299778e0f224084c796
Igor Beduin <igor.rob.96@gmail.com>
2019-05-23 01:30:02 -0300

    TitleStage as initial stage

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,12 @@
 #include "../include/Game.h"
-#include "../include/StageState.h"
+#include "../include/TitleState.h"
 
 int main(int argc, char** argv) {
     Game& game = Game::GetInstance();
 
-    StageState* initialState = new StageState();
+    TitleState* initialState = new TitleState();
     
-    game.Push((State*)initialState);
+    game.Push(initialState);
     game.Run();
 
     return 0;
