e093e71dc459efd899437ce6c5ab5a798ea8c5ac
Igor Beduin <igor.rob.96@gmail.com>
2019-05-24 20:57:13 -0300

    EndState done

--- include/EndState.h
+++ include/EndState.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#define WIN_SCREEN_PATH "assets/img/win.jpg"
+#define LOSE_SCREEN_PATH "assets/img/lose.jpg"
+#define TITLE_FONT_PATH "assets/font/Call me maybe.ttf"
+
 #include "State.h"
 #include "Music.h"
+#include "GameObject.h"
+#include "Sprite.h"
+#include "Text.h"
+#include "TitleState.h"
+#include "StageState.h"
 
 class EndState : public State
 {
--- src/EndState.cpp
+++ src/EndState.cpp
@@ -1,34 +1,85 @@
 #include "../include/EndState.h"
 #include "../include/GameData.h"
+#include "../include/Game.h"
+#include "../include/InputManager.h"
+
 
 EndState::EndState() : State::State()
 {   
-    // TODO: component de text que diz para o jogador 
-    // apertar ESC para voltar para o menu ou SPACEBAR
-    // para jogar novamente
+    GameObject* endScreen = new GameObject();
+    GameObject* endMessage = new GameObject();
 
+    Sprite* endScreenSprite;
+    Text* endMessageText;
+
+    SDL_Color endTextColor;
+    std::string conditinalText;
     if (GameData::playerVictory)
     {
-        // TODO: objeto de vitoria
-
+        endScreenSprite = new Sprite(*endScreen, WIN_SCREEN_PATH);
+        conditinalText = "YES! You have won!";
+        endTextColor = {0, 200, 0, 0};
     }
     else
     {
-        // TODO: objeto de derrota
+        endScreenSprite = new Sprite(*endScreen, LOSE_SCREEN_PATH);
+        conditinalText = "Oh no! You have lost!";
+        endTextColor = {200, 0, 0, 0};
     }
+    std::string text = conditinalText + "\n" + "Press ESC to go to Menu or SPACEBAR to play again";
+    endMessageText = new Text(*endMessage, TITLE_FONT_PATH, 50, Text::BLENDED, text, endTextColor, 2);
+
+    endScreen->AddComponent((std::shared_ptr<Component>)endScreenSprite);
+    endMessage->AddComponent((std::shared_ptr<Component>)endMessageText);
+
+    AddObject(endScreen);
+    AddObject(endMessage);
+
 }
 
 void EndState::LoadAssets()
 {}
 void EndState::Update(float dt)
 {
-    // TODO: Verificar condições de vitoria e derrota
+    if (InputManager::GetInstance().QuitRequested())
+    {
+        quitRequested = true;
+    }
+    if (InputManager::GetInstance().KeyPress(ESCAPE_KEY))
+    {
+        TitleState* title = new TitleState();
+        Game::GetInstance().Push(title);
+        popRequested = true;
+    }
+    if (InputManager::GetInstance().KeyPress(SPACEBAR_KEY))
+    {   
+        StageState* stage = new StageState();
+        Game::GetInstance().Push(stage);
+        popRequested = true;
+    }
+
+    for (int i = 0; i != (int)objectArray.size(); i++)
+    {
+        objectArray[i]->Update(dt);
+    }
 }
 void EndState::Render()
-{}
+{
+    for (int i = 0; i != (int)objectArray.size(); i++)
+    {
+        objectArray[i]->Render();
+    }
+}
 
 void EndState::Start()
-{}
+{
+    LoadAssets();
+    for (int i = 0; i < (int)objectArray.size(); i++)
+    {
+        objectArray[i]->Start();
+    }
+    started = true;
+}
 void EndState::Pause()
 {}
 void EndState::Resume()
